40ef0f6e89272c887394ddb9bef38e181d64f093
niklasschmitz <niklas.schmitz@outlook.de>
2018-10-26 18:49:22 +0200

    fill ex05

--- labs/ex05/template/least_squares.py
+++ labs/ex05/template/least_squares.py
@@ -14,4 +14,11 @@ def least_squares(y, tx):
     # least squares: TODO
     # returns mse, and optimal weights
     # ***************************************************
-    raise NotImplementedError
+    a = tx.T.dot(tx)
+    b = tx.T.dot(y)
+    w = np.linalg.solve(a,b)
+
+    N = y.size
+    e = y - tx.dot(w)
+    mse = 1/ (2*N) * np.vdot(e,e) 
+    return mse, w
